085f5e6d1a0678a17ee8d667dac16671946a07db
Churchil Owino <chachowino75@gmail.com>
2022-11-01 16:46:51 +0300

    Add JavaScript to highlight navbar on scroll

---
+++ index.js
@@ -0,0 +1,31 @@
+// Highlighting the navbar based on the section currently being scrolled
+
+// First, grab the navbar and the sections 
+
+const navLi = document.querySelectorAll('nav ul li');
+const sections = document.querySelectorAll('section');
+
+// Add an event listener to listen for scroll
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if(scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLi.forEach( li => {
+        li.classList.remove('active');
+
+        if(li.classList.contains(current)) {
+            li.classList.add('active');
+        }
+    })
+})
+
+
